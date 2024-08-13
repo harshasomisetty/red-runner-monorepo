3e2691fe59c3b4701ac177dcb6e9fb685fed53c9
UmerButtCLV <umer.butt@clvtechnologies.com>
2024-08-13 19:11:23 +0500

    Shop Windows Created. UI Manager Added.

--- Assets/Scripts/Shop/ShopManager.cs
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -4,10 +4,13 @@ using UnityEngine.UI;
 
 public class ShopManager : MonoBehaviour
 {
-    public static GameShopDecoder Instance;
+    public static ShopManager Instance;
+
     [Header("UI elements")]
     public Transform SpeedBoosterContainer;
-    public Transform DoubleJumpContainer, SkinContainer;
+    public Transform DoubleJumpContainer;
+    public Transform SkinContainer;
+
     [Header("Detail Panel UI")]
     public GameObject DetailPanel;
     public TextMeshProUGUI attribute;
@@ -16,95 +19,135 @@ public class ShopManager : MonoBehaviour
     public Image boosterImage;
 
 
-    public GameShop _gameShop;
+    //Shop Data Availability//
+    bool ShopDataAvailability = false;
 
-    private void OnEnable()
+    public GameShop GameShopData;
+    void Start()
     {
-        _gameShop = GameShopDecoder.Instance.GameShop;
+        API_Manager.instance.GetShopData(GetAllShopData);
     }
+
+    void GetAllShopData(bool sucess, string response)
+    {
+        if (sucess)
+        {
+            GameShop _gamedata = JsonUtility.FromJson<GameShop>(response);
+            GameShopData = _gamedata;
+            ShopDataAvailability = true;
+        }
+        else
+        {
+            ShopDataAvailability = false;
+            Debug.Log("Shop Data Failed");
+        }
+        ConfirmShopDataAvailability(ShopDataAvailability);
+    }
+
+    void ConfirmShopDataAvailability(bool State)
+    {
+        if (State)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
     public void GetSkinsDetails(int index)
     {
         DetailPanel.SetActive(true);
         switch (index)
         {
             case 0:
-                GameShop.Skin zombieSkin = _gameShop.skins.zombieSkin;
+                GameShop.Skin zombieSkin = GameShopData.skins.zombieSkin;
                 ShowDetailPanel(zombieSkin);
                 break;
             case 1:
-                GameShop.Skin alienSkin = _gameShop.skins.alienSkin;
+                GameShop.Skin alienSkin = GameShopData.skins.alienSkin;
                 ShowDetailPanel(alienSkin);
                 break;
             case 2:
-                GameShop.Skin robotSkin = _gameShop.skins.robotSkin;
+                GameShop.Skin robotSkin = GameShopData.skins.robotSkin;
                 ShowDetailPanel(robotSkin);
                 break;
             case 3:
-                GameShop.Skin spaceSkin = _gameShop.skins.spaceSkin;
+                GameShop.Skin spaceSkin = GameShopData.skins.spaceSkin;
                 ShowDetailPanel(spaceSkin);
                 break;
             case 4:
-                GameShop.Skin christmasSkin = _gameShop.skins.christmasSkin;
+                GameShop.Skin christmasSkin = GameShopData.skins.christmasSkin;
                 ShowDetailPanel(christmasSkin);
                 break;
             case 5:
-                GameShop.Skin solanaSkin = _gameShop.skins.solanaSkin;
+                GameShop.Skin solanaSkin = GameShopData.skins.solanaSkin;
                 ShowDetailPanel(solanaSkin);
                 break;
             case 6:
-                GameShop.Skin halloweenSkin = _gameShop.skins.halloweenSkin;
+                GameShop.Skin halloweenSkin = GameShopData.skins.halloweenSkin;
                 ShowDetailPanel(halloweenSkin);
                 break;
             case 7:
-                GameShop.Skin polkaDotSkin = _gameShop.skins.polkaDotSkin;
+                GameShop.Skin polkaDotSkin = GameShopData.skins.polkaDotSkin;
                 ShowDetailPanel(polkaDotSkin);
                 break;
             case 8:
-                GameShop.Skin thiefSkin = _gameShop.skins.thiefSkin;
+                GameShop.Skin thiefSkin = GameShopData.skins.thiefSkin;
                 ShowDetailPanel(thiefSkin);
                 break;
             case 9:
-                GameShop.Skin wrestlerSkin = _gameShop.skins.wrestlerSkin;
+                GameShop.Skin wrestlerSkin = GameShopData.skins.wrestlerSkin;
                 ShowDetailPanel(wrestlerSkin);
                 break;
-
         }
     }
     
-    public void GetBoostersDetails(int index)
+    public void GetSpeedBoostersDetails(int index)
     {
         DetailPanel.SetActive(true);
         switch (index)
         {
             case 0:
-                GameShop.Booster speed = _gameShop.boosters.speed_boosters.speed_booster_3;
+                GameShop.Booster speed = GameShopData.boosters.speed_boosters.speed_booster_3;
                 ShowDetailPanel(speed);
                 break;
             case 1:
-                GameShop.Booster speed1 = _gameShop.boosters.speed_boosters.speed_booster_6;
+                GameShop.Booster speed1 = GameShopData.boosters.speed_boosters.speed_booster_6;
                 ShowDetailPanel(speed1);
                 break;
             case 2:
-                GameShop.Booster speed2 = _gameShop.boosters.speed_boosters.speed_booster_10;
+                GameShop.Booster speed2 = GameShopData.boosters.speed_boosters.speed_booster_10;
                 ShowDetailPanel(speed2);
                 break;
             case 3:
-                GameShop.Booster speed3 = _gameShop.boosters.speed_boosters.speed_booster_999;
+                GameShop.Booster speed3 = GameShopData.boosters.speed_boosters.speed_booster_999;
                 ShowDetailPanel(speed3);
                 break;
-            case 4:
-                GameShop.Booster jump = _gameShop.boosters.double_jump_boosters.double_jump_3;
+        }
+    }
+
+    public void GetJumpBoostersDetails(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                GameShop.Booster jump = GameShopData.boosters.double_jump_boosters.double_jump_3;
                 ShowDetailPanel(jump);
                 break;
-            case 5:
-                GameShop.Booster jump1 = _gameShop.boosters.double_jump_boosters.double_jump_6;
+            case 1:
+                GameShop.Booster jump1 = GameShopData.boosters.double_jump_boosters.double_jump_6;
                 ShowDetailPanel(jump1);
                 break;
-            case 6:
-                GameShop.Booster jump3 = _gameShop.boosters.double_jump_boosters.double_jump_999;
+            case 2:
+                GameShop.Booster jump2 = GameShopData.boosters.double_jump_boosters.double_jump_10;
+                ShowDetailPanel(jump2);
+                break;
+            case 3:
+                GameShop.Booster jump3 = GameShopData.boosters.double_jump_boosters.double_jump_999;
                 ShowDetailPanel(jump3);
                 break;
-
         }
     }
 
@@ -125,6 +168,7 @@ public class ShopManager : MonoBehaviour
         Description.text = skin.description;
         attribute.text = skin.attributes[0].traitType + ":" + skin.attributes[0].value;
     }
+
     public void ShowDetailPanel(GameShop.Booster booster)
     {
         API_Manager.instance.DownloadImage(booster.imageUrl, (success, sprite) => {
